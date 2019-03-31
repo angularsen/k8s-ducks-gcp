472036ec24c211ea940d5f26df74d9f39a09c22a
Andreas Gullberg Larsen <andreas.larsen84@gmail.com>
2019-03-31 21:31:16 +0200

    Change to managed node pool and micro instances

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "google_container_cluster" "gcp_kubernetes" {
+resource "google_container_cluster" "primary" {
     initial_node_count  = "${var.cluster_instance_count}"
     name                = "${var.cluster_name}"
     location            = "${var.gcp_location}"
@@ -9,7 +9,23 @@ resource "google_container_cluster" "gcp_kubernetes" {
         password        = "${var.linux_admin_password}"
     }
 
+    # We can't create a cluster with no node pool defined, but we want to only use
+    # separately managed node pools. So we create the smallest possible default
+    # node pool and immediately delete it.
+    remove_default_node_pool = true
+    initial_node_count = 1
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+    name                = "${var.cluster_name}-node-pool"
+    location            = "${var.gcp_location}"
+    cluster             = "${google_container_cluster.primary.name}"
+    node_count          = 1
+
     node_config {
+        preemptible     = true
+        machine_type    = "f1-micro"
+
         oauth_scopes    = [
             "https://www.googleapis.com/auth/compute",
             "https://www.googleapis.com/auth/devstorage.read_only",
